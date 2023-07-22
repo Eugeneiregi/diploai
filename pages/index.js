fa2d6d1ab524b93601e31eca923a4be3d40b1829
Eugene <eugeneregi2019@gmail.com>
2023-07-22 23:17:32 +0300

    Intial commit

--- pages/index.js
+++ pages/index.js
@@ -1,117 +1,111 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useState } from 'react';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [additionalContent, setAdditionalContent] = useState(false);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen items-center justify-center">
+      <div
+        className="bg-white p-8 rounded-lg shadow-md space-y-4"
+        style={{
+          width: '422px',
+          height: '494px',
+          position: 'relative',
+          left: '10px',
+          right: '10px',
+          top: '24px',
+          background: '#ECE6F0',
+          borderRadius: '18px',
+          boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1',
+        }}
+      >
+      <div className="space-y-2">
+          <h1 className="text-4xl font-bold"
+              style={{
+                fontFamily: 'Roboto',
+                fontWeight: 400,
+                fontSize: '24px',
+                lineHeight: '20px',
+                color: '#000000',
+                letterSpacing: '0.25px',
+              }}
+              >DiploAI</h1>
+          <p className="text-lg"
+          style={{
+            color: '#49454F',
+            fontSize: '15px'
+          }}
+              
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            DiploAI is a tool that helps companies and governments deal with increasing climate complexity and regulatory change, by using GenAI to identify their optimal strategy to achieve a given goal.
+          </p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        {additionalContent && (
+          <div className="mt-4 space-y-2">
+            <ul className="space-y-2">
+              <li className="flex items-center">
+                <div
+                  className="w-8 h-8 rounded-full bg-purple-600 text-white flex items-center justify-center mr-2"
+                  style={{ background: '#D3D3D3', color: 'purple' }}
+                >
+                  A
+                </div>
+                List item
+                <div className="flex-grow" />
+                <span className="text-purple-600">100+</span>
+                <input type="checkbox" className="ml-2 w-4 h-4 bg-purple-600 rounded" />
+              </li>
+              <hr className="my-2 border-gray-400" />
+              <hr />
+              <li className="flex items-center">
+                <div
+                  className="w-8 h-8 rounded-full bg-purple-600 text-white flex items-center justify-center mr-2"
+                  style={{ background: '#D3D3D3', color: 'purple' }}
+                >
+                  A
+                </div>
+                List item
+                <div className="flex-grow" />
+                <span className="text-purple-600">100+</span>
+                <input type="checkbox" className="ml-2 w-4 h-4 bg-purple-600 rounded" />
+              </li>
+              <hr className="my-2 border-gray-400" />
+              <hr />
+              <li className="flex items-center">
+                <div
+                  className="w-8 h-8 rounded-full bg-purple-600 text-white flex items-center justify-center mr-2"
+                  style={{ background: '#D3D3D3', color: 'purple' }}
+                >
+                  A
+                </div>
+                List item
+                <div className="flex-grow" />
+                <span className="text-purple-600">100+</span>
+                <input type="checkbox" className="ml-2 w-4 h-4 bg-purple-600 rounded"
+                style={{  color: 'purple' }} />
+              </li>
+            </ul>
+          </div>
+        )}
+        <button
+          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+          onClick={() => setAdditionalContent(!additionalContent)}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {additionalContent ? 'Hide List' : 'Show List'}
+        </button>
+        <div className="absolute bottom-4 right-4 flex items-right">
+          <h1 className="text-lg font-semibold" style={{ color: 'purple', cursor: 'pointer' }}>
+            Action-1
+          </h1>
+          <h1 className="text-lg font-semibold ml-4" style={{ color: 'purple', cursor: 'pointer' }}>
+            Action-2
+          </h1>
+        </div>
       </div>
     </main>
   )
